f29efc8b32c7f5e8308d6d3dd068bcf8bef8316f
J.-Fabian Wenisch <fabian@wenisch.tech>
2021-10-05 13:26:29 +0200

    fix: configurable vars

--- main.go
+++ main.go
@@ -6,28 +6,59 @@ import (
     "net/http"
 	"os"
 	"io"
+	"io/ioutil"
+	"encoding/json"
+
 	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
 	
 )
 
 
+type Todo struct {
+    Url      string    `json:"url"`
+    Filename string      `json:"filename"`
+}
 
 func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
+    fmt.Fprintf(w, "Successfully procesed!")
     fmt.Println("Endpoint Hit: homePage")
 
-	fileUrl := "https://rss.cluster.wenisch.tech/klenkes/events/ics"
-	err := DownloadFile("calendar.ics", fileUrl)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Downloaded: " + fileUrl)
+	//Start JSON decoding
+	 var todo Todo
+    body, errUnmarshal := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+    if errUnmarshal != nil {
+        panic(errUnmarshal)
+    }
+    if errUnmarshal := r.Body.Close(); errUnmarshal != nil {
+        panic(errUnmarshal)
+    }
+    if errUnmarshal := json.Unmarshal(body, &todo); errUnmarshal != nil {
+        w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+        w.WriteHeader(422) // unprocessable entity
+        if errUnmarshal := json.NewEncoder(w).Encode(errUnmarshal); errUnmarshal != nil {
+            panic(errUnmarshal)
+        }
+    }
+ 
+    
+	    fmt.Println("Fetching from " +todo.Url);
+		fmt.Println("Downloading as " +todo.Filename);
+	// END JSON decoding
 
-user   := ""
-	pass   := ""
-	remote := ""
-	port   := ":22"
+
+	//START DOWNLOAD
+	errDownload := DownloadFile(todo.Filename, todo.Url)
+	if errDownload != nil {
+		panic(errDownload)
+	}
+	//END DOWNLOAD
+	fmt.Println("Sucessfully downloaded: " + todo.Url)
+
+	user   := os.Getenv("D2SFTP_USER")
+	pass   := os.Getenv("D2SFTP_PASS")
+	remote := os.Getenv("D2SFTP_REMOTE")
+	port   := os.Getenv("D2SFTP_PORT")
 
 	
 
@@ -41,7 +72,8 @@ user   := ""
 	}
 
 	// connect
-	conn, err := ssh.Dial("tcp", remote+port, config)
+	fmt.Println("Connecting to " +remote+":"+port +" as "+user);
+	conn, err := ssh.Dial("tcp", remote+":"+port, config)
 	if err != nil {
 	   log.Fatal(err)
 	}
@@ -55,14 +87,14 @@ user   := ""
 	defer client.Close()
 
 	// create destination file
-	dstFile, err := client.Create("./file.txt")
+	dstFile, err := client.Create("./"+todo.Filename)
 	if err != nil {
 	   log.Fatal(err)
 	}
 	defer dstFile.Close()
 
 	// create source file
-	srcFile, err := os.Open("./file.txt")
+	srcFile, err := os.Open(todo.Filename)
 	if err != nil {
 	   log.Fatal(err)
 	}
